b02e8896e3b21f5253af1793cc5c4c73d62d8f6e
Priyanka Priyadarsini Bej <219680202+Priyanka11-source@users.noreply.github.com>
2026-09-05 01:16:23 +0530

    Time: 0 ms (100%), Space: 8 MB (36.12%) - LeetHub

---
+++ 2235-add-two-integers/2235-add-two-integers.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        if(!(-100<=num1 && num2<=100))
+            return 0;
+        int add;
+        add=num1+num2;
+        return add;
+    }
+};
