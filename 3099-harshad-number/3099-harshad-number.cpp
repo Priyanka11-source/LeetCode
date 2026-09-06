d555ebce54f15804b7e7042edede0abecfb7cb6b
Priyanka Priyadarsini Bej <219680202+Priyanka11-source@users.noreply.github.com>
2026-09-07 00:55:29 +0530

    Time: 0 ms (100%), Space: 7.9 MB (50.14%) - LeetHub

---
+++ 3099-harshad-number/3099-harshad-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int sumOfTheDigitsOfHarshadNumber(int x) {
+        int sum=0, temp=x;
+        while(x>0)
+        {
+                int digit=x%10;
+                sum+=digit;
+                x/=10;
+        }
+        if(temp%sum==0)
+            return sum;
+        else
+            return -1;
+    }
+};
